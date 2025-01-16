94446ad1ef230d0eb1f12d86f0a0cf1b629288e9
Sandra Misigo <misigosandra@gmail.com>
2025-01-17 02:01:55 +0300

    react-hooks-lists-and-keys-lab

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,34 @@ import ProjectList from "./ProjectList";
 import user from "../data/user";
 
 function App() {
+
+const projects = [
+  {
+    id: 1,
+    name: "Reciplease",
+    about: "A recipe tracking app",
+    technologies: ["Rails", "Bootstrap CSS"],
+  },
+  {
+    id: 2,
+    name: "Kibbles N Bitz",
+    about: "Tinder for dogs",
+    technologies: ["React", "Redux"],
+  },
+  {
+    id: 3,
+    name: "Alienwares",
+    about: "Etsy for aliens",
+    technologies: ["React", "Redux", "Rails"],
+  },
+];
+
   return (
     <div>
       <NavBar />
       <Home username={user.name} city={user.city} color={user.color} />
       <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <ProjectList projects={projects} />
     </div>
   );
 }
